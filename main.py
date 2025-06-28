b3c82478bf73c1e751594307e687eeabb9e2cd7e
Golu171 <111356117+Golu171@users.noreply.github.com>
2025-06-29 01:57:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,12 +54,12 @@ async def show_random_emojis(message):
     return emoji_message
     
 # Define the owner's user ID
-OWNER_ID = 5840594311 # Replace with the actual owner's user ID
+OWNER_ID = 5543709855 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [5840594311]
+SUDO_USERS = [5543709855]
 
-AUTH_CHANNEL = -1002572301679
+AUTH_CHANNEL = -1001965444840
 
 # Function to check if a user is authorized
 def is_authorized(user_id: int) -> bool:
